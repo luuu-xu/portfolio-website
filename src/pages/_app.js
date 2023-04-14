b2025a627878f487a6380dee60523cd5fa2ec3f4
luuu-xu <a550763321@gmail.com>
2023-04-14 13:43:56 -0400

    Added Audience Analytics from Vercel

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,15 @@
 import Layout from '@/components/layout'
 import '@/styles/globals.css'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   // return <Component {...pageProps} />
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      <Analytics />
+    </>
   );
 }
