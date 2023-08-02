e63bcb7428716203925fc60badb2bb6c260c2645
luuu-xu <a550763321@gmail.com>
2023-08-02 13:38:06 -0400

    Added global tailwind custom styles

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
       </Head>
-      <body className='text-black bg-white dark:text-gray-200 dark:bg-neutral-900'>
+      <body>
         <Main />
         <NextScript />
       </body>
